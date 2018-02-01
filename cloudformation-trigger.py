e663fee4b0c5efe7615bd383bd749d17b69401c5
Nithin Liviero <688727@student.tor.loc>
2018-02-01 15:20:10 -0500

    Adding lambda trigger code

---
+++ cloudformation-trigger.py
@@ -0,0 +1,9 @@
+def lambda_handler(event, context):
+    # TODO implement
+    import boto3
+    print 'start execution'
+    cf_client = boto3.client('cloudformation')
+    cf_client.create_stack(
+    StackName='VPC-30',
+    TemplateURL='https://s3.amazonaws.com/cloudformation-template-lambda-bucket/vpc.yaml'
+    )
