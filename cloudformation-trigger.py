5bb30ab9b3f48d58b3b7e98a86c5d2d09328d906
Nithin Liviero <688727@student.tor.loc>
2018-02-01 15:33:12 -0500

    restoring to old version

--- cloudformation-trigger.py
+++ cloudformation-trigger.py
@@ -4,8 +4,6 @@ def lambda_handler(event, context):
     print 'start execution'
     cf_client = boto3.client('cloudformation')
     cf_client.create_stack(
-    StackName='VPC-30',
-    please remove this line------
-    
+    StackName='VPC-30',    
     TemplateURL='https://s3.amazonaws.com/cloudformation-template-lambda-bucket/vpc.yaml'
     )
